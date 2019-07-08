df939262e864ddb9b8c53596f558ca9e766ba7ed
Andrei Yasinski <andrewtopol856@gmail.com>
2019-07-08 11:52:36 +0300

    add project

---
+++ src/components/addForm/addForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './addForm.module.css';
+
+export default class AddForm extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        
+      </div>
+    )
+  }
+}
---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import AddForm from '../addForm/addForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AddForm />
+    </div>
+  );
+}
+
+export default App;
