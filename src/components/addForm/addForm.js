e7484b072babc3a9e9681eaa7943e4d2fdfe558c
Andrei Yasinski <andrewtopol856@gmail.com>
2019-07-08 18:41:27 +0300

    add addForm component

--- src/components/addForm/addForm.js
+++ src/components/addForm/addForm.js
@@ -1,11 +1,58 @@
 import React from 'react';
 import styles from './addForm.module.css';
+import RecipeItem from '../recipeItem/recipeItem';
+import defaultImage from '../../assets/default-image.jpg';
 
 export default class AddForm extends React.Component {
+  state = {
+    name: '',
+    time: '',
+    image: defaultImage,
+    recipes : []
+  }
+
+  handleChange = (e) => {
+    console.log(this.state.name)
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      recipes: [{name: this.state.name, time: this.state.time, image: defaultImage}, ...this.state.recipes]
+    })
+  }
+
   render() {
     return (
-      <div className={styles.container}>
-        
+      <div>
+        <form className={styles.add_form} onSubmit={this.handleSubmit}>
+          <input
+            name = 'name'
+            type='text'
+            placeholder='Название...'
+            className={styles.input}
+            value = {this.state.name}
+            onChange = {this.handleChange}
+          />
+          <input
+            name = 'time'
+            type='text'
+            placeholder='Время готовки...'
+            className={styles.input}
+            value = {this.state.time}
+            onChange = {this.handleChange}
+          />
+          <input 
+            type='file'
+            className={styles.add_image}
+            accept='image/jpeg,image/png'
+          />
+          <button className={styles.button}>Добавить рецепт</button>
+        </form>
+        <RecipeItem />
       </div>
     )
   }
---
+++ src/components/recipeItem/recipeItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './recipeItem.module.css';
+
+const RecipeItem = props => {
+  return (
+    <div></div>
+  )
+}
+
+export default RecipeItem;
